9275375e464191226b569c9449aed7c80c9acb90
AmirahChamble <AmirahChamble@users.noreply.github.com>
2021-08-24 15:47:34 +0000

    Deploying to main from @ Walgreens-LSG/LSG_React@c65cf867fbf28aeb29486b67b5851bfbd6bf5e30 🚀

--- components/Alert/AlertDescription.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "@emotion/styled";
-import PropTypes from "prop-types";
-import { AlertDescription } from "@chakra-ui/react";
-export const WagAlertDescription = ({ children, ...props }) => {
-  const WagAlertDescription = styled(AlertDescription)``;
-
-  return <WagAlertDescription {...props}>{children}</WagAlertDescription>;
-};
